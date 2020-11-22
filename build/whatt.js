8d62c185399048111f08074193ab0885ebb76f5f
Bastiaan <bjlvandenberg@gmail.com>
2020-11-22 13:57:13 +0100

    Updated constants due to Whatsapp update.

--- build/whatt.js
+++ build/whatt.js
@@ -181,9 +181,9 @@ class Client {
   };
 };
 
-const getTextBox = () => document.querySelector('#main > footer > div._3ee1T._1LkpH.copyable-area > div._3uMse > div > div._3FRCZ.copyable-text.selectable-text');
-const getSendButton = () => document.querySelector('#main > footer > div._3ee1T._1LkpH.copyable-area > div:nth-child(3) > button');
-const getChat = () => document.querySelector('#main > div._3h-WS > div > div > div.z_tTQ');
+const getTextBox = () => document.querySelector('#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text');
+const getSendButton = () => document.querySelector('#main > footer > div._3SvgF._1mHgA.copyable-area > div:nth-child(3) > button');
+const getChat = () => document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1');
 const getChats = () => document.querySelector('#pane-side > div:nth-child(1) > div > div');
 
 /**
